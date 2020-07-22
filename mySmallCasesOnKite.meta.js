904c1194ced9a86d9c6c8cf2248dde28cec98294
Amit Rana <amit.rana@gmail.com>
2020-07-22 19:05:40 +0530

    version up

--- mySmallCasesOnKite.meta.js
+++ mySmallCasesOnKite.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         mySmallCasesOnKite
 // @namespace    http://mySmallCasesOnKite.net/
-// @version      0.2
+// @version      0.3
 // @description  Introduces small features on top of kite app
 // @author       Amit
 // @match        https://kite.zerodha.com/*
