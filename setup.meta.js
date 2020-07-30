f6fda8c0b7cb74a757b249d0d07598116e5a3328
Amit Rana <amit.rana@gmail.com>
2020-07-30 13:41:45 +0530

    setup file

---
+++ setup.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         mySmallCasesOnKite
+// @namespace    http://mySmallCasesOnKite.net/
+// @version      0.1
+// @description  Introduces small features on top of kite app
+// @author       Amit
+// @match        https://kite.zerodha.com/*
+// @grant        none
+// @downloadURL  https://github.com/amit0rana/betterKite/raw/master/mySmallCasesOnKite.user.js
+// @updateURL    https://github.com/amit0rana/betterKite/raw/master/mySmallCasesOnKite.meta.js
+// ==/UserScript==
