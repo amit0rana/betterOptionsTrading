9366bdde42895d002f372c22d702a1c5b9372f36
Amit Rana <amit.rana@gmail.com>
2020-11-19 10:04:12 +0530

    version up

--- betterSensibull.user.js
+++ betterSensibull.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         betterSensibull
 // @namespace    https://github.com/amit0rana/betterSensibull
-// @version      0.02
+// @version      0.03
 // @description  Introduces small features on top of sensibull
 // @author       Amit
 // @match        https://web.sensibull.com/*
