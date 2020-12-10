4355bc8d85a9468fda411ca798854a7ef60f2cf9
Amit Rana <amit.rana@gmail.com>
2020-12-10 14:48:07 +0530

    moved p&l to bottom of the table.

--- betterKite.user.js
+++ betterKite.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         betterKite
 // @namespace    https://github.com/amit0rana/betterKite
-// @version      2.10
+// @version      2.11
 // @description  Introduces small features on top of kite app
 // @author       Amit
 // @match        https://kite.zerodha.com/*
@@ -19,7 +19,7 @@
 var context=window,options="{    anonymizeIp: true,    colorDepth: true,    characterSet: true,    screenSize: true,    language: true}";const hhistory=context.history,doc=document,nav=navigator||{},storage=localStorage,encode=encodeURIComponent,pushState=hhistory.pushState,typeException="exception",generateId=()=>Math.random().toString(36),getId=()=>(storage.cid||(storage.cid=generateId()),storage.cid),serialize=e=>{var t=[];for(var o in e)e.hasOwnProperty(o)&&void 0!==e[o]&&t.push(encode(o)+"="+encode(e[o]));return t.join("&")},track=(e,t,o,n,i,a,r)=>{const c="https://www.google-analytics.com/collect",s=serialize({v:"1",ds:"web",aip:options.anonymizeIp?1:void 0,tid:"UA-176741575-1",cid:getId(),t:e||"pageview",sd:options.colorDepth&&screen.colorDepth?`${screen.colorDepth}-bits`:void 0,dr:doc.referrer||void 0,dt:doc.title,dl:doc.location.origin+doc.location.pathname+doc.location.search,ul:options.language?(nav.language||"").toLowerCase():void 0,de:options.characterSet?doc.characterSet:void 0,sr:options.screenSize?`${(context.screen||{}).width}x${(context.screen||{}).height}`:void 0,vp:options.screenSize&&context.visualViewport?`${(context.visualViewport||{}).width}x${(context.visualViewport||{}).height}`:void 0,ec:t||void 0,ea:o||void 0,el:n||void 0,ev:i||void 0,exd:a||void 0,exf:void 0!==r&&!1==!!r?0:void 0});if(nav.sendBeacon)nav.sendBeacon(c,s);else{var d=new XMLHttpRequest;d.open("POST",c,!0),d.send(s)}},tEv=(e,t,o,n)=>track("event",e,t,o,n),tEx=(e,t)=>track(typeException,null,null,null,null,e,t);hhistory.pushState=function(e){return"function"==typeof history.onpushstate&&hhistory.onpushstate({state:e}),setTimeout(track,options.delay||10),pushState.apply(hhistory,arguments)},track(),context.ma={tEv:tEv,tEx:tEx};
 
 window.jQ=jQuery.noConflict(true);
-const VERSION = "v2.10";
+const VERSION = "v2.11";
 const PRO_MODE = false;
 const GM_HOLDINGS_NAME = "BK_HOLDINGS";
 const GMPositionsName = "BK_POSITIONS";
@@ -1365,14 +1365,13 @@ function main() {
 
             }
             var t = ceQ+"CE & "+peQ+"PE";
+            var pnlText = "";
             if (pnl > 0) {
-                //jQ("div.positions > section.open-positions.table-wrapper > div > div > table > thead > tr > th.select > div > label").append(
-                //jQ(jQ("div.positions > section.open-positions.table-wrapper > div > div > table > tfoot > tr > td")[1]).append(
-                jQ("div.positions > section.open-positions.table-wrapper > header").append(
-                    "<span random-att='temppnl' class='text-green open pnl randomClassToHelpHide'>P&L: "+formatter.format(pnl)+"<span class='text-label randomClassToHelpHide'>Max: "+formatter.format(maxPnl)+"</span></span>");
+                //jQ("div.positions > section.open-positions.table-wrapper > header").append(
+                pnlText = "<span random-att='temppnl' class='text-green open pnl randomClassToHelpHide'>P&L: "+formatter.format(pnl)+"<span class='text-label randomClassToHelpHide'>Max: "+formatter.format(maxPnl)+"</span></span>";
             } else {
-                jQ("div.positions > section.open-positions.table-wrapper > header").append(
-                    "<span random-att='temppnl' class='text-red open pnl randomClassToHelpHide'>P&L: "+formatter.format(pnl)+"<span class='text-label randomClassToHelpHide'>Max: "+formatter.format(maxPnl)+"</span></span>");
+                //jQ("div.positions > section.open-positions.table-wrapper > header").append(
+                pnlText = "<span random-att='temppnl' class='text-red open pnl randomClassToHelpHide'>P&L: "+formatter.format(pnl)+"<span class='text-label randomClassToHelpHide'>Max: "+formatter.format(maxPnl)+"</span></span>";
             }
 
             var mTag = jQ("span[random-att='marginsave']");
@@ -1380,6 +1379,7 @@ function main() {
                 mTag.remove();
             }
             jQ(jQ("div.positions > section.open-positions.table-wrapper > div > div > table > tfoot > tr > td")[0]).append("<span random-att='marginsave' class='pnl randomClassToHelpHide'> "+t+" (Points: "+formatter.format(points)+")</span>");
+            jQ(jQ("div.positions > section.open-positions.table-wrapper > div > div > table > tfoot > tr > td")[0]).append(pnlText);
         }
 
     });
