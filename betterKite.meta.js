86a4a5bde39dfeae74196b6a3087fd890e09a098
Amit Rana <amit.rana@gmail.com>
2020-12-31 08:49:28 +0530

    Removed common.ks

--- betterKite.meta.js
+++ betterKite.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         betterKite
 // @namespace    https://github.com/amit0rana/betterKite
-// @version      2.11
+// @version      2.12
 // @description  Introduces small features on top of kite app
 // @author       Amit
 // @match        https://kite.zerodha.com/*
@@ -10,7 +10,6 @@
 // @grant        GM_addStyle
 // @grant        GM_registerMenuCommand
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js
-// @require      https://github.com/amit0rana/betterOptionsTrading/raw/master/common.js
 // @require      https://raw.githubusercontent.com/amit0rana/MonkeyConfig/master/monkeyconfig.js
 // @downloadURL  https://github.com/amit0rana/betterOptionsTrading/raw/master/betterKite.user.js
 // @updateURL    https://github.com/amit0rana/betterOptionsTrading/raw/master/betterKite.meta.js
