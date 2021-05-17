1995d9dca7e65265d37ddf7e37072c19115c9ebd
Amit Rana <amit.rana@gmail.com>
2021-05-17 06:37:48 +0530

    bug fix

--- common.js
+++ common.js
@@ -2,19 +2,16 @@ const D_LEVEL_INFO = 2;
 const D_LEVEL_DEBUG = 1;
 
 const log = function(level, logInfo) {
-    if (level >= D_LEVEL) {
-        switch(level) {
-            case D_LEVEL_DEBUG:
-                console.debug(logInfo);
-              break;
-            case D_LEVEL_INFO:
-                console.info(logInfo);
-              break;
-            default:
-                console.log(logInfo);
-          } 
-        
-    }
+    switch(level) {
+        case D_LEVEL_DEBUG:
+            console.debug(logInfo);
+            break;
+        case D_LEVEL_INFO:
+            console.info(logInfo);
+            break;
+        default:
+            console.log(logInfo);
+        }
 }
 const debug = function(logInfo) {
     log( D_LEVEL_DEBUG , logInfo);
