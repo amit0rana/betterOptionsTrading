769b1cff579eef44cc9e2e9a26b11451532a6ac4
Amit Rana <amit.rana@gmail.com>
2021-05-17 06:34:57 +0530

    bug fix

--- common.js
+++ common.js
@@ -3,14 +3,24 @@ const D_LEVEL_DEBUG = 1;
 
 const log = function(level, logInfo) {
     if (level >= D_LEVEL) {
-        console.log(logInfo);
+        switch(level) {
+            case D_LEVEL_DEBUG:
+                console.debug(logInfo);
+              break;
+            case D_LEVEL_INFO:
+                console.info(logInfo);
+              break;
+            default:
+                console.log(logInfo);
+          } 
+        
     }
 }
 const debug = function(logInfo) {
     log( D_LEVEL_DEBUG , logInfo);
 }
 const info = function(logInfo) {
-    log( D_LEVEL_INFO , logInfo);
+    log( D_LEVEL_INFO, logInfo);
 }
 const formatter = Intl.NumberFormat('en-IN', { 
     style: 'currency', currency: 'INR'
