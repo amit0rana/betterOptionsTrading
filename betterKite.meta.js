12a387204963f44f70b4020096c9e6c17f49e32a
Amit Rana <amit.rana@gmail.com>
2021-06-25 07:51:40 +0530

    bug fix

--- betterKite.meta.js
+++ betterKite.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         betterKite
 // @namespace    https://github.com/amit0rana/betterKite
-// @version      3.13
+// @version      3.14
 // @description  Introduces small features on top of kite app
 // @author       Amit
 // @match        https://kite.zerodha.com/*
