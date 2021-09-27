798239ed4845bd486efea0fa64dc24c60ae9aaea
Amit Rana <amit.rana@gmail.com>
2021-09-27 11:17:35 +0530

    hiding positions

--- betterKite.meta.js
+++ betterKite.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         betterKite
 // @namespace    https://github.com/amit0rana/betterKite
-// @version      3.28
+// @version      3.29
 // @description  Introduces small features on top of kite app
 // @author       Amit
 // @match        https://kite.zerodha.com/*
