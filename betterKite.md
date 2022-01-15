dd5e0400e9fc4b255ed570db3769cfa399fb2943
Amit Rana <amit.rana@gmail.com>
2022-01-15 18:59:00 +0530

    added license

--- betterKite.md
+++ betterKite.md
@@ -1,5 +1,15 @@
 # betterKite
 
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or distribute this software, either in source code form or as a compiled binary, for any purpose, commercial or non-commercial, and by any means.
+
+In jurisdictions that recognize copyright laws, the author or authors of this software dedicate any and all copyright interest in the software to the public domain. We make this dedication for the benefit of the public at large and to the detriment of our heirs and successors. We intend this dedication to be an overt act of relinquishment in perpetuity of all present and future rights to this software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to [<https://unlicense.org>](https://unlicense.org)
+
 Very simple "userscript" which adds several features to kite.zerodha.com ui
 * 'Smart Limit' Place LIMIT orders at best offer/bid price.
 * Avoid quantity freeze limitation, breaks bigger order into multiple orders
@@ -157,3 +167,26 @@ This script tracks following events for analytics. No data is passed/stored/trac
 * Tag added
 * Tag deleted
 * Filter applied
+
+[//]: # (SEBI MANDATORY DISCLAIMER AS REQUIRED BY SEBI)
+
+[//]: # (KIND ATTENTION TO ALL USERS)
+
+[//]: # (Disclaimer from Developers as per SEBI norms: Equity Investment are subject to 100%)
+[//]: # (market risks. Refer your financial consultant advice before Investing. This code is)
+[//]: # (only for Educational and Learning, Knowledge Purposes. Developers have no)
+[//]: # (responsibility for your intended decision & financial losses. Keep calculated & always)
+[//]: # (analyzed your cash osition and risk bearing capacity before following any advise.)
+[//]: # (Stock market investments and using this script are VERY RISKY and using ths code, you )
+[//]: # (agree that you understand the Market risks involved. Profits and Losses are part of Share market.)
+[//]: # (Kindly understand and act wisely.)
+
+[//]: # (Disclaimer/ disclosure)
+
+[//]: # (👉This script does not provide any tips/recommendations/advice)
+[//]: # (👉All features are only for education and learning purpose.)
+[//]: # (👉Do Consult your financial advisor before taking trades or investment decisions)
+[//]: # (👉Developers are not responsible for any financial losses)
+[//]: # (👉Disclaimer/disclosure/terms and conditions applicable to all users of this script)
+
+[//]: # (All the features are for education and learning purpose only👍)
--- betterKite.user.js
+++ betterKite.user.js
@@ -24,6 +24,31 @@
 // @updateURL    https://github.com/amit0rana/betterOptionsTrading/raw/master/betterKite.meta.js
 // ==/UserScript==
 
+// This is free and unencumbered software released into the public domain.
+
+// Anyone is free to copy, modify, publish, use, compile, sell, or
+// distribute this software, either in source code form or as a compiled
+// binary, for any purpose, commercial or non-commercial, and by any
+// means.
+
+// In jurisdictions that recognize copyright laws, the author or authors
+// of this software dedicate any and all copyright interest in the
+// software to the public domain. We make this dedication for the benefit
+// of the public at large and to the detriment of our heirs and
+// successors. We intend this dedication to be an overt act of
+// relinquishment in perpetuity of all present and future rights to this
+// software under copyright law.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+// OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+
+// For more information, please refer to <https://unlicense.org>
+
 const my_css = GM_getResourceText("TOASTIFY_CSS");
 GM_addStyle(my_css);
 
@@ -2803,7 +2828,6 @@ function sensibull(firstTry = true) {
         rows.each(function (rowIndex) {
             var t = jQ(this).find('th > div > div:nth-child(3)').text().split(" ")[2];
 
-            //console.log('bs: text to cmp' + t.toUpperCase());
             debug('toggle for ' + t);
             if (selectedItem == 'All' || t.toUpperCase() == selectedItem.toUpperCase()) {
                 debug('toggle : success');
@@ -2953,6 +2977,7 @@ function addOverrideOption() {
         i.onclick = function () {
             debug(jQ("#overQtyFreezeCb").is(':checked'));
             _overrideQtyFreeze = jQ("#overQtyFreezeCb").is(':checked');
+            tEv("kite", "place-order", "override-qt-freeze", "");
         };
 
         _overrideQtyFreeze = false;
@@ -3009,14 +3034,14 @@ function sendReplacement(data) {
                 button.click();
             }, 0);
             
-            console.log('aborting send ' + data);
+            console.log('aborting send ');
 
             this.abort();
             
             if (jQ("#smartLimit").is(":checked")) {
                 _smartLimit = true;
                 jQ("#smartLimit").prop('checked', false);
-
+                tEv("kite", "place-order", "smart-limit", "");
                 // throw new Error("Smart Limit Order is executed in the background, Check orders. Check betterKite message");
             }  else {
                 _smartLimit = false;
@@ -3212,3 +3237,27 @@ jQ.fn.exists = function () {
 }
 
 tEv("kite", "visit", "main", VERSION);
+
+
+// SEBI MANDATORY DISCLAIMER AS REQUIRED BY SEBI
+
+// KIND ATTENTION TO ALL USERS
+
+// Disclaimer from Developers as per SEBI norms: Equity Investment are subject to 100% 
+// market risks. Refer your financial consultant advice before Investing. This code is
+//  only for Educational and Learning, Knowledge Purposes. Developers have no 
+// responsibility for your intended decision & financial losses. Keep calculated & always 
+// analyzed your cash osition and risk bearing capacity before following any advise.
+//  Stock market investments and using this script are VERY RISKY and using ths code, you 
+// agree that you understand the Market risks involved. Profits and Losses are part of Share market. 
+//  Kindly understand and act wisely.
+
+// Disclaimer/ disclosure
+
+// 👉This script does not provide any tips/recommendations/advice
+// 👉All features are only for education and learning purpose.
+// 👉Do Consult your financial advisor before taking trades or investment decisions
+// 👉Developers are not responsible for any financial losses
+// 👉Disclaimer/disclosure/terms and conditions applicable to all users of this script
+
+// All the features are for education and learning purpose only👍
