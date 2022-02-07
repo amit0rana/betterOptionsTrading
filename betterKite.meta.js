a45c1e46df440c17e0b87fea23e4ea4b94cf0df8
Amit Rana <amit.rana@gmail.com>
2022-02-07 10:44:41 +0530

    removed irritaing row flicker on selection

--- betterKite.meta.js
+++ betterKite.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         betterKite
 // @namespace    https://github.com/amit0rana/betterKite
-// @version      3.42
+// @version      3.43
 // @description  Introduces small features on top of kite app
 // @author       Amit
 // @match        https://kite.zerodha.com/*
