a6389ca40b5de3890ea95b7ad36dbd731812ff36
Amit Rana <amit.rana@gmail.com>
2022-11-22 10:21:41 +0530

    auto refresh bug

--- betterKite.user.js
+++ betterKite.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         betterKite
 // @namespace    https://github.com/amit0rana/betterKite
-// @version      3.56
+// @version      3.57
 // @description  Introduces small features on top of kite app
 // @author       Amit
 // @match        https://kite.zerodha.com/*
@@ -60,7 +60,7 @@ GM_addStyle(my_css);
 var context = window, options = "{    anonymizeIp: true,    colorDepth: true,    characterSet: true,    screenSize: true,    language: true}"; const hhistory = context.history, doc = document, nav = navigator || {}, storage = localStorage, encode = encodeURIComponent, pushState = hhistory.pushState, typeException = "exception", generateId = () => Math.random().toString(36), getId = () => (storage.cid || (storage.cid = generateId()), storage.cid), serialize = e => { var t = []; for (var o in e) e.hasOwnProperty(o) && void 0 !== e[o] && t.push(encode(o) + "=" + encode(e[o])); return t.join("&") }, track = (e, t, o, n, i, a, r) => { const c = "https://www.google-analytics.com/collect", s = serialize({ v: "1", ds: "web", aip: options.anonymizeIp ? 1 : void 0, tid: "UA-176741575-1", cid: getId(), t: e || "pageview", sd: options.colorDepth && screen.colorDepth ? `${screen.colorDepth}-bits` : void 0, dr: doc.referrer || void 0, dt: doc.title, dl: doc.location.origin + doc.location.pathname + doc.location.search, ul: options.language ? (nav.language || "").toLowerCase() : void 0, de: options.characterSet ? doc.characterSet : void 0, sr: options.screenSize ? `${(context.screen || {}).width}x${(context.screen || {}).height}` : void 0, vp: options.screenSize && context.visualViewport ? `${(context.visualViewport || {}).width}x${(context.visualViewport || {}).height}` : void 0, ec: t || void 0, ea: o || void 0, el: n || void 0, ev: i || void 0, exd: a || void 0, exf: void 0 !== r && !1 == !!r ? 0 : void 0 }); if (nav.sendBeacon) nav.sendBeacon(c, s); else { var d = new XMLHttpRequest; d.open("POST", c, !0), d.send(s) } }, tEv = (e, t, o, n) => track("event", e, t, o, n), tEx = (e, t) => track(typeException, null, null, null, null, e, t); hhistory.pushState = function (e) { return "function" == typeof history.onpushstate && hhistory.onpushstate({ state: e }), setTimeout(track, options.delay || 10), pushState.apply(hhistory, arguments) }, track(), context.ma = { tEv: tEv, tEx: tEx };
 
 window.jQ = jQuery.noConflict(true);
-const VERSION = "v3.56";
+const VERSION = "v3.57";
 const GM_HOLDINGS_NAME = "BK_HOLDINGS";
 const GMPositionsName = "BK_POSITIONS";
 const GMRefTradeName = "BK_REF_TRADES";
@@ -462,6 +462,10 @@ function getSensibullZerodhaTradingSymbol(original) {
     //BHARTIARTL JUN FUT NFO
     debug(`getSensibullZerodhaTradingSymbol ${original}`)
     var ts = original.split(" ");
+    debug(`count ${ts.length}`)
+
+    if (ts.length == 1) return original;
+
     if (ts[2] == 'FUT') {
         debug('FUT');
         return `${ts[0]} ${getLastThursday(ts[1])} ${ts[2]}`;
@@ -1502,7 +1506,7 @@ function showPositionDropdown(retry = true) {
     if (g_config.get('auto_refresh_PnL') === true) {
         debug('going to observe pnl change');
         target = jQ("div.positions > section.open-positions.table-wrapper > div > div > table > tfoot > tr > td")[3];
-        div > section.open-positions.table-wrapper > div > div > table > tfoot > tr > td.text-right.text-green
+        
         debug("**********" + jQ(target).text());
         g_positionsPnlObserver = new MutationObserver(function (mutations) {
             var st = null;
@@ -1544,6 +1548,7 @@ function getScripExpiryText(fullText) {
 function getExpiryText(fullText) {
     //debug('getExpiryText ' + fullText);
     var expiry = fullText.split(' ');
+    if (expiry.length == 1) return "";
     var t = expiry[1];
 
     return t;
@@ -1926,6 +1931,8 @@ function updatePnl(forPositions = true) {
     var selection = [];
     allVisibleRows.each(function (rowIndex) {
         var v = jQ(jQ(this).find("td")[pnlCol]).text().split(",").join("");
+        
+        if (v == "") return;
         pnl += parseFloat(v);
 
         if (forPositions) {
